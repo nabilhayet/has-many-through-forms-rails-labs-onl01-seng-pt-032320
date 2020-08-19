173bf82ca55214a34c4f78bd69e6298354cf875d
nabilhayet <nabil4457@yahoo.com>
2020-08-18 21:39:51 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name], users_attributes: [:username], users_attributes: [:password], comment_content: [:content])
+    params.require(:post).permit(:title, :content, category_ids:[], categories_attributes: [:name])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,18 +5,13 @@ class Post < ActiveRecord::Base
   has_many :users, through: :comments
 
   def categories_attributes=(category_attributes)
-    category_attributes.values.each do |category_attribute|
+      category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)
       self.categories << category
     end
   end
 
-  def users_attributes=(user_attributes)
-    user_attributes.values.each do |user_attribute|
-      user = User.find_or_create_by(user_attribute)
-      self.users << user 
-    end
-  end
   
+
 
 end
